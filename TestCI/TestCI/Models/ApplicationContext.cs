3ac1d49ace8f4933f4a63e9f67a256f3cba6598a
Stanislav Svehlik <stanislav.svehlik@gmail.com>
2022-12-13 17:30:23 +0100

    setup project include all packages

---
+++ TestCI/TestCI/Models/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestCI.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ TestCI/TestCI/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using TestCI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//var myconfig = builder.Configuration["Myconfig"];
+
+builder.Services.AddControllersWithViews();
+
+//builder.Services.AddTransient<IPostService, PostService>();
+//builder.Services.AddTransient<IUserService, UserService>();
+//builder.Services.AddTransient<IVoteService, VoteService>();
+//builder.Services.AddTransient<ISessionService, SessionService>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession();
+var config = builder.Services.BuildServiceProvider().GetService<IConfiguration>();
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
+app.MapControllers();
+
+app.Run();
